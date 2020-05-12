0caf0e0d89000949a09df18cfd94ff45914f5afb
DevL0per <vazhnik.roma@gmail.com>
2020-05-12 12:44:48 +0300

    Initial Commit

---
+++ appleWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appleWeatherApp
+//
+//  Created by Роман Важник on 12.05.2020.
+//  Copyright © 2020 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
